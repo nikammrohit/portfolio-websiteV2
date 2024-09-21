50cfbec48b84ee4f6a803af0a7b3bc1b56b0bca7
nikammrohit <82987914+nikammrohit@users.noreply.github.com>
2024-09-20 22:19:52 -0500

    Removed unnecessary import on App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import Header from './Header';
 import LandingPage from './LandingPage';
-import Projects from './Projects';
-import About from './About';
 import Contact from './Contact';
 import Footer from './Footer';
 import '../App.css';
