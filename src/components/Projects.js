2b4e9d95e30dfeec2a47fc544f1fd8495ce4db5b
nikammrohit <82987914+nikammrohit@users.noreply.github.com>
2024-09-20 23:57:28 -0500

    Added titles to Projects and links to each project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,11 +2,12 @@ import React from 'react';
 import '../styles/Projects.css';
 
 const projects = [
-  { id: 1, title: 'Project 1', description: 'Description of project 1' },
-  { id: 2, title: 'Project 2', description: 'Description of project 2' },
-  { id: 3, title: 'Project 3', description: 'Description of project 3' },
-  { id: 4, title: 'Project 4', description: 'Description of project 4' },
-  { id: 5, title: 'Project 5', description: 'Description of project 5' },
+  { id: 1, title: 'Automated Data Manipulation Tool', description: 'Description of project 1', url: 'https://github.com/nikammrohit/Automated-ZView'},
+  { id: 2, title: 'Realtime Messaging Application', description: 'Description of project 2', url: 'https://github.com/nikammrohit/Chat-App' },
+  { id: 3, title: 'Facial Recognition Software', description: 'Description of project 3', url: 'https://github.com/nikammrohit/Image-Facial-Recognition' },
+  { id: 4, title: 'Expense Tracker Web Application', description: 'Description of project 4', url: 'https://github.com/nikammrohit/Expense-Tracker' },
+  { id: 5, title: 'Task Manager IOS App', description: 'Description of project 5', url: 'https://github.com/nikammrohit/Animated-To-Do-App' },
+  { id: 6, title: 'Movie Catalog Website', description: 'Description of project 6', url: 'https://github.com/nikammrohit/Small-Movie-Catelog' },
   // Add more projects as needed
 ];
 
@@ -16,10 +17,10 @@ const Projects = () => {
 	  <h2>My Projects</h2>
 	  <div className="projects-container">
 		{projects.map((project) => (
-		  <div key={project.id} className="project-card">
+		  <a key={project.id} href={project.url} className="project-card" target="_blank" rel="noopener noreferrer">
 			<h3>{project.title}</h3>
 			<p>{project.description}</p>
-		  </div>
+		  </a>
 		))}
 	  </div>
 	</section>
