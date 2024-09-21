dc803213917c6c3f8d69a87082676b6884674602
nikammrohit <82987914+nikammrohit@users.noreply.github.com>
2024-09-20 23:14:01 -0500

    Updated glowing line location

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,17 +6,27 @@ const Header = () => {
 		<header className="header">
 			<div className="logo">
 				<a href="#landing-page">My Portfolio</a>
-				<div className="glowing-lines">
-        			<div className="line red"></div>
-        			<div className="line yellow"></div>
-        			<div className="line green"></div>
-      			</div>
 			</div>
 			<nav className="nav">
 				<ul>
-					<li><a href="#about">About</a></li>
-					<li><a href="#projects">Projects</a></li>
-					<li><a href="#contact">Contact</a></li>
+					<li>
+						<a href="#about">About</a>
+						<div className="glowing-lines">
+							<div className="line red"></div>
+						</div>
+					</li>
+					<li>
+						<a href="#projects">Projects</a>
+						<div className="glowing-lines">
+							<div className="line yellow"></div>
+						</div>
+					</li>
+					<li>
+						<a href="#contact">Contact</a>
+						<div className="glowing-lines">
+							<div className="line green"></div>
+						</div>
+					</li>
 				</ul>
 			</nav>
 		</header>
